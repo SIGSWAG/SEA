d79ec847693eb63cb6502b8d96943c5d9cbc1732
Halepian Thibault <thalepian@if501-219-19.insa-lyon.fr>
2015-12-01 11:53:13 +0100

    definitions

--- sysif/src/vmem.h
+++ sysif/src/vmem.h
@@ -1,6 +1,11 @@
 #ifndef VMEM_H
 #define VMEM_H
 
+#define PAGE_SIZE 4096 //4 kBytes
+#define FIRST_LVL_TT_COUNT 4096 //12 bits
+#define FIRST_LVL_TT_SIZE 16384 //4096*4 Bytes
+#define SECON_LVL_TT_COUNT 256 //8 bits
+#define SECON_LVL_TT_SIZE 1024 // 256*4 Bytes
 
 
 #endif
