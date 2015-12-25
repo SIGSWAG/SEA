ad03c6fc60e3dc103b1f6be11259fadc4936952c
Loïc Touzard <loic.touzard@gmail.com>
2015-12-25 18:58:37 +0100

    Leap motion application. Detection and sending serial messages ok. :matrice: "not tested"

--- LeapMotion/SEA.py
+++ LeapMotion/SEA.py
@@ -1,41 +1,86 @@
 import Leap, sys, thread, time, serial
-from Leap import CircleGesture, KeyTapGesture, ScreenTapGesture, SwipeGesture
+from Leap import SwipeGesture
 
 class SerialController(serial.Serial):
-        
+
+    def __init__(self, testing = False):
+        super(SerialController, self).__init__()
+        self.testing = testing
+
     def open(self):
-        super(SerialController, self).open()
+        if self.testing:
+            print "Testing : "
+        else:
+            super(SerialController, self).open()
         print "Serial communication oppened at port %s with baudrate at %d" % (self.portstr, self.baudrate)
 
     def close(self):
-        super(SerialController, self).open()
+        if self.testing:
+            print "Testing : "
+        else:
+            super(SerialController, self).open()
         print "Serial communication closed"
 
     def write(self, string):
-        super(SerialController, self).write(string)
+        if self.testing:
+            print "Testing : "
+        else:
+            super(SerialController, self).write(string)
         print "Message '%s' has been sent" % string
 
 class LeapMotionForMusicListener(Leap.Listener):
     finger_names = ['Thumb', 'Index', 'Middle', 'Ring', 'Pinky']
     bone_names = ['Metacarpal', 'Proximal', 'Intermediate', 'Distal']
     state_names = ['STATE_INVALID', 'STATE_START', 'STATE_UPDATE', 'STATE_END']
-    serial = None
+
+    @property
+    def app_width(self):
+        return self._app_width
+    
+    @property
+    def app_height(self):
+        return self._app_height
+    
+    @property
+    def app_depth(self):
+        return self._app_depth
+    
+    @property
+    def detection_percentage(self):
+        return self._detection_percentage
+    
+    @property
+    def detection_width(self):
+        return self._detection_percentage * self.app_width
+
+    @property
+    def detection_height(self):
+        return self._detection_percentage * self.app_height
+
+    @property
+    def detection_depth(self):
+        return self._detection_percentage * self.app_depth    
+
+
 
     def __init__(self, serial):
         super(LeapMotionForMusicListener, self).__init__()
-        self.serial = serial
+        self._serial = serial
+        # measures in mm
+        self._app_width = 600
+        self._app_height = 400
+        self._app_depth = 400
+        self._detection_percentage = 0.5
 
     def on_init(self, controller):
-        self.serial.open()
+        if not self._serial.isOpen():
+            self._serial.open()
         print "Initialized"
 
     def on_connect(self, controller):
         print "Connected"
 
         # Enable gestures
-        controller.enable_gesture(Leap.Gesture.TYPE_CIRCLE);
-        controller.enable_gesture(Leap.Gesture.TYPE_KEY_TAP);
-        controller.enable_gesture(Leap.Gesture.TYPE_SCREEN_TAP);
         controller.enable_gesture(Leap.Gesture.TYPE_SWIPE);
 
     def on_disconnect(self, controller):
@@ -43,15 +88,23 @@ class LeapMotionForMusicListener(Leap.Listener):
         print "Disconnected"
 
     def on_exit(self, controller):
-        self.serial.close()
+        self._serial.close()
         print "Exited"
 
     def on_frame(self, controller):
         # Get the most recent frame and report some basic information
         frame = controller.frame()
+        messages = []
 
         print "Frame id: %d, timestamp: %d, hands: %d, fingers: %d, tools: %d, gestures: %d" % (
               frame.id, frame.timestamp, len(frame.hands), len(frame.fingers), len(frame.tools), len(frame.gestures()))
+
+        # Interaction box
+        i_box = frame.interaction_box
+        
+        # exit if there is more than one hand
+        if len(frame.hands) != 1:
+            return
 
         # Get hands
         for hand in frame.hands:
@@ -61,9 +114,34 @@ class LeapMotionForMusicListener(Leap.Listener):
             print "  %s, id %d, position: %s" % (
                 handType, hand.id, hand.palm_position)
 
+            normalized_palm_position = i_box.normalize_point(hand.palm_position)
+            # Normalized position is between [0,1]
+            # Scale and center
+            position_x = (self.app_width  * normalized_palm_position.x) - (self.app_width / 2)
+            position_y = (self.app_height * (1 - normalized_palm_position.y)) - (self.app_height / 2)
+            position_z = (self.app_depth * normalized_palm_position.z) - (self.app_depth / 2)
+
             # Get the hand's normal vector and direction
             normal = hand.palm_normal
             direction = hand.direction
+
+            diff_x = _outsideX(position_x)
+            diff_y = _outsideX(position_y)
+            diff_z = _outsideX(position_z)
+
+            if diff_x < 0:
+                messages.append("left")
+            elif diff_x > 0:
+                messages.append("right")
+            if diff_y < 0:
+                messages.append("forward")
+            elif diff_y > 0:
+                messages.append("backward")
+            if diff_z < 0:
+                messages.append("up")
+            elif diff_z > 0:
+                messages.append("down")
+            
 
             # Calculate the hand's pitch, roll, and yaw angles
             print "  pitch: %f degrees, roll: %f degrees, yaw: %f degrees" % (
@@ -71,78 +149,45 @@ class LeapMotionForMusicListener(Leap.Listener):
                 normal.roll * Leap.RAD_TO_DEG,
                 direction.yaw * Leap.RAD_TO_DEG)
 
-            # Get arm bone
-            arm = hand.arm
-            print "  Arm direction: %s, wrist position: %s, elbow position: %s" % (
-                arm.direction,
-                arm.wrist_position,
-                arm.elbow_position)
-
-            # Get fingers
-            for finger in hand.fingers:
-
-                print "    %s finger, id: %d, length: %fmm, width: %fmm" % (
-                    self.finger_names[finger.type],
-                    finger.id,
-                    finger.length,
-                    finger.width)
-
-                # Get bones
-                for b in range(0, 4):
-                    bone = finger.bone(b)
-                    print "      Bone: %s, start: %s, end: %s, direction: %s" % (
-                        self.bone_names[bone.type],
-                        bone.prev_joint,
-                        bone.next_joint,
-                        bone.direction)
-
-        # Get tools
-        for tool in frame.tools:
-
-            print "  Tool id: %d, position: %s, direction: %s" % (
-                tool.id, tool.tip_position, tool.direction)
-
         # Get gestures
         for gesture in frame.gestures():
-            if gesture.type == Leap.Gesture.TYPE_CIRCLE:
-                circle = CircleGesture(gesture)
-
-                # Determine clock direction using the angle between the pointable and the circle normal
-                if circle.pointable.direction.angle_to(circle.normal) <= Leap.PI/2:
-                    clockwiseness = "clockwise"
-                else:
-                    clockwiseness = "counterclockwise"
-
-                # Calculate the angle swept since the last frame
-                swept_angle = 0
-                if circle.state != Leap.Gesture.STATE_START:
-                    previous_update = CircleGesture(controller.frame(1).gesture(circle.id))
-                    swept_angle =  (circle.progress - previous_update.progress) * 2 * Leap.PI
-
-                print "  Circle id: %d, %s, progress: %f, radius: %f, angle: %f degrees, %s" % (
-                        gesture.id, self.state_names[gesture.state],
-                        circle.progress, circle.radius, swept_angle * Leap.RAD_TO_DEG, clockwiseness)
-
             if gesture.type == Leap.Gesture.TYPE_SWIPE:
                 swipe = SwipeGesture(gesture)
                 print "  Swipe id: %d, state: %s, position: %s, direction: %s, speed: %f" % (
                         gesture.id, self.state_names[gesture.state],
                         swipe.position, swipe.direction, swipe.speed)
 
-            if gesture.type == Leap.Gesture.TYPE_KEY_TAP:
-                keytap = KeyTapGesture(gesture)
-                print "  Key Tap id: %d, %s, position: %s, direction: %s" % (
-                        gesture.id, self.state_names[gesture.state],
-                        keytap.position, keytap.direction )
-
-            if gesture.type == Leap.Gesture.TYPE_SCREEN_TAP:
-                screentap = ScreenTapGesture(gesture)
-                print "  Screen Tap id: %d, %s, position: %s, direction: %s" % (
-                        gesture.id, self.state_names[gesture.state],
-                        screentap.position, screentap.direction )
-
         if not (frame.hands.is_empty and frame.gestures().is_empty):
             print ""
+
+        if len(messages):
+            for message in messages:
+                self._serial.write(message)
+
+    
+    def _outsideX(self, x):
+        limit = self.detection_width / 2
+        if x <= -limit :
+            return x + limit
+        elif x >= limit :
+            return x - limit
+        return 0
+    
+    def _outsideY(self, y):
+        limit = self.detection_height / 2
+        if y <= -limit :
+            return y + limit
+        elif y >= limit :
+            return y - limit
+        return 0
+
+    def _outsideZ(self, z):
+        limit = self.detection_depth / 2
+        if z <= -limit :
+            return z + limit
+        elif z >= limit :
+            return z - limit
+        return 0
 
     def state_string(self, state):
         if state == Leap.Gesture.STATE_START:
--- LeapMotion/main.py
+++ LeapMotion/main.py
@@ -3,15 +3,21 @@ import SEA
 
 PORT = 0
 BAUDRATE = 9400
+SWIPE_MINLENGTH = 100.0		# mm
+SWIPE_MINVELOCITy = 750		# mm/s
 
 def main():
-
-	serialController = SEA.SerialController();
-	serialController.port = 0
-	serialController.baudrate = 9400
+	serialController = SEA.SerialController(testing=True);
+	serialController.port = PORT
+	serialController.baudrate = BAUDRATE
 	# Create a sample listener and controller
 	listener = SEA.LeapMotionForMusicListener(serialController)
 	controller = Leap.Controller()
+
+	# Configure SwipeGesture detection
+	controller.config.set("Gesture.Swipe.MinLength", SWIPE_MINLENGTH)
+	controller.config.set("Gesture.Swipe.MinVelocity", SWIPE_MINVELOCITy)
+	controller.config.save()
 
 	# Have the sample listener receive events from the controller
 	controller.add_listener(listener)
