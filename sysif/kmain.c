f48ab3d0f98b9b8cf119a06bd162efabe6531d25
HugoDelval <hugo.delval@insa-lyon.fr>
2016-01-10 13:51:10 +0100

    actions leap motion uniquement si musique en lecture.

--- sysif/kmain.c
+++ sysif/kmain.c
@@ -19,58 +19,65 @@ void serialReceiver()
         // bloquant mais attente non active
         uart_receive_str(msg, 2);
         // detection du caractere reçu
-        switch(msg[0]){
-            case 'L':
-                // Left
-                // Ralentir
-                diminuer_vitesse();
-                break;
-            case 'R':
-                // Right
-                // Accelerer
-                augmenter_vitesse();
-                break;
-            case 'U':
-                // Up
-                musique_stop();
-                break;
-            case 'D':
+        if(musique_est_arretee())
+        {  
+            if(msg[0] == 'D')
+            {
                 // Down
-				// Play / pause
-				if(musique_est_arretee()){
-					musique_lecture();
-				}
-				else{
-					musique_pause();
-				}
-                break;
-            case 'F':
-                // Forward
-                musique_suivante();
-                break;
-            case 'B':
-                // Backward
-                musique_precedente();
-                break;
-            case '+':
-                // Circle clockwise
-                // Monter le son
-                augmenter_volume();
-                break;
-            case '-':
-                // Circle counterclockwise
-                // Baisser le son
-                diminuer_volume();
-                break;
-            case 'I':
-                // FistClosed
-				break;
-			case 'O':
-				// FistOpened
-				break;
-			default:
-				break;
-		}
+                // Play
+                musique_lecture();
+            }
+        }
+        else
+        {
+            switch(msg[0]){
+                case 'L':
+                    // Left
+                    // Ralentir
+                    diminuer_vitesse();
+                    break;
+                case 'R':
+                    // Right
+                    // Accelerer
+                    augmenter_vitesse();
+                    break;
+                case 'U':
+                    // Up
+                    musique_stop();
+                    break;
+                case 'D':
+                    // Down
+    				// Pause
+    				musique_pause();
+                    break;
+                case 'F':
+                    // Forward
+                    musique_suivante();
+                    break;
+                case 'B':
+                    // Backward
+                    musique_precedente();
+                    break;
+                case '+':
+                    // Circle clockwise
+                    // Monter le son
+                    augmenter_volume();
+                    break;
+                case '-':
+                    // Circle counterclockwise
+                    // Baisser le son
+                    diminuer_volume();
+                    break;
+                case 'I':
+                    // FistClosed
+    				break;
+    			case 'O':
+    				// FistOpened
+    				break;
+    			default:
+    				break;
+    		}
+        }
 	}
 }
 
